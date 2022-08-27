a4f8fd4f9b228e19ce860c7d77ae290de92d991e
Skraevern <86933999+Skraevern@users.noreply.github.com>
2022-08-27 23:20:42 +0200

    Added all neaded functions. Works

--- tick-tack-toe.rb
+++ tick-tack-toe.rb
@@ -15,34 +15,91 @@ class Game
         puts "#############################"
         puts "####### Tick Tack Toe #######"
         puts "#############################"
-        puts "#{@game_pos_array[1]} | #{@game_pos_array[2]} | #{@game_pos_array[3]}     "
-        puts "#{@game_pos_array[4]} | #{@game_pos_array[5]} | #{@game_pos_array[6]}"
+        puts "#{@game_pos_array[1]} | #{@game_pos_array[2]} | #{@game_pos_array[3]}     X : #{@p1.name}"
+        puts "#{@game_pos_array[4]} | #{@game_pos_array[5]} | #{@game_pos_array[6]}     O : #{@p2.name}"    
         puts "#{@game_pos_array[7]} | #{@game_pos_array[8]} | #{@game_pos_array[9]}"
     end
     def play
         while !@game_over
+            clear
+            show_board
             puts "#{@active_player.name} pick a number!"
             @picked_number = gets.chomp
-            
-            ## Check if input is a number between 1 and 9
-            while !@picked_number.to_i.between?(1,9)
-                puts "Wrong number try agin"
+
+            # Check if input is a number
+            while @picked_number.to_i == 0
+                puts "Not a number. Try again"
                 @picked_number = gets.chomp
             end
             
-            @game_over = true
+            ## Check if input is a number that is left in array
+            while !@game_pos_array.include?(@picked_number.to_i)
+                puts "Wrong number try agin"
+                @picked_number = gets.chomp
+            end
+            puts "Correct"
+
+            ## Adds active player symbol to correct position
+            @game_pos_array.each_with_index { |num, i|  
+                if num == @picked_number.to_i
+                    @game_pos_array[i] = @active_player.symbol
+                end
+            }
+
+            @active_player.picked_numbers_array.push(@picked_number.to_i)
+
+            check_winner
         end
     end
 
+    def check_winner
+        win_arr = [
+            [1, 2, 3],
+            [4, 5, 7],
+            [7, 8, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 6, 9],
+            [1, 5, 9],
+            [3, 5, 7]
+          ]
+        win_arr.each { |arr| 
+            score = 0
+            arr.each_with_index { |num, i| 
+                if num == @active_player.picked_numbers_array[i]
+                    score = score + 1
+                end
+                if score == 3
+                    display_winner
+                end
+            }
+        }
+
+        if @active_player.player_number == 1
+            @p1 = @active_player
+            @active_player = @p2
+        else
+            @p2 = @active_player
+            @active_player = @p1
+        end
+    end
+
+    def display_winner
+        clear
+        show_board
+        puts "Congratulations #{@active_player.name}!"
+        @game_over = true
+    end
 end
 
 class Player
-    attr_accessor :name
-    def initialize(player_number)
+    attr_reader :name, :symbol, :player_number
+    attr_accessor :picked_numbers_array
+    def initialize(player_number, symbol)
         @player_number = player_number
-        @picked_nubers_array = []
+        @picked_numbers_array = []
         @name = nil
-        @symbol = nil
+        @symbol = symbol
         ask_names
     end
     def ask_names
@@ -62,6 +119,6 @@ clear()
 puts "#############################"
 puts "####### Tick Tack Toe #######"
 puts "#############################"
-p1 = Player.new(1)
-p2 = Player.new(2)
+p1 = Player.new(1, "X")
+p2 = Player.new(2, "O")
 game = Game.new(p1, p2)
